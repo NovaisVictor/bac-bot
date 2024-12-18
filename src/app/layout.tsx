7282bdde9848894743e3961357655da4a4449f8a
NovaisVictor <victornovaislima@gmail.com>
2024-12-18 14:39:50 -0300

    remove pix infinit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Click Cash',
+  title: 'Bac Bot',
   description: 'Clique e aprenda a lucrar',
 }
 
